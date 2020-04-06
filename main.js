43270eb5ae8387ae3a08c97a9322a268032d6391
Jokery2007 <61429464+Jokery2007@users.noreply.github.com>
2020-04-06 20:14:21 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 alert('Witaj na stronie Photoshopów aby przejść dalej kliknij w przycisk poniżej. Nie zapomnij wpaść na podstrone z Recenzjami Gier');
+console.log('Witaj na stronie Photoshopów aby przejść dalej kliknij w przycisk poniżej. Nie zapomnij wpaść na podstrone z Recenzjami Gier');
+let imie = 'Daniel';
+let wiek = 13
+console.log(`Mam na imie ${name} i mam ${wiek} lat `);
