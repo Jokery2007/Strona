cbf9063d221e818e10e71366030fe89603dc55ea
Jokery2007 <61429464+Jokery2007@users.noreply.github.com>
2020-04-20 21:40:23 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ alert( `Witaj ${name}` );
 console.log(name);
 let wiek = prompt ('Wiek też potrzebuje : (Jest on wykorzystywany do analizy czy mogę cię wpuścić na tę stronę)')
 if(wiek=12,11,10,9,8,7,6,5,4,3,2,1){
-alert('Ta strona nie jest przeznaczona dla Ciebię poczekaj do ukończenia 13 roku życia (Wchodzisz tu na własną odpowiedzialność)')
-}
-else if(wiek<-1){
-alert('Bardzo śmieszne wpisz poprawny wiek')
+alert('Ta strona nie jest przeznaczona dla Ciebię poczekaj do ukończenia 13 roku życia s')
+window.close()
 }
 else{
     alert('Zapraszamy do smrodowa tu od smrodu boli głowa')
