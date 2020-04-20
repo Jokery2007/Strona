c6556088fd1342b510a1ee807dae79336d1744a4
Jokery2007 <61429464+Jokery2007@users.noreply.github.com>
2020-04-20 21:02:54 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ let name = prompt ('Jeśli mogę wiedzieć podaj swoje imię : ');
 alert( `Witaj ${name}` );
 console.log(name);
 let wiek = prompt ('Wiek też potrzebuje : (Jest on wykorzystywany do analizy czy mogę cię wpuścić na tę stronę)')
-if(wiek<13){
+if(wiek=12,11,10,9,8,7,6,5,4,3,2,1){
 alert('Ta strona nie jest przeznaczona dla Ciebię poczekaj do ukończenia 13 roku życia (Wchodzisz tu na własną odpowiedzialność)')
+}
+else if(wiek<-1){
+alert('Bardzo śmieszne wpisz poprawny wiek')
 }
 else{
     alert('Zapraszamy do smrodowa tu od smrodu boli głowa')
