b6720bc44d25fbe40c73ef2f995889982e647d47
Jokery2007 <61429464+Jokery2007@users.noreply.github.com>
2020-05-14 10:42:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,5 @@
 let name = prompt ('Jeśli mogę wiedzieć podaj swoje imię : ');
 alert( `Witaj ${name}` );
 console.log(name);
-let wiek = prompt ('Wiek też potrzebuje : (Jest on wykorzystywany do analizy czy mogę cię wpuścić na tę stronę)')
-if(wiek=12,11,10,9,8,7,6,5,4,3,2,1){
-alert('Ta strona nie jest przeznaczona dla Ciebie poczekaj do ukończenia 13 roku życia')
-window.close()
-}
-else{
     alert('Zapraszamy do smrodowa tu od smrodu boli głowa')
 }
